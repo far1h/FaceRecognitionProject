49bb44fa631106d8f2eb23eef8e270095a0bbd42
far1h <farihmuhammad45@gmail.com>
2024-09-18 21:01:59 +0700

    Add Face ID functionality

--- FaceRecognitionProject/ViewController.swift
+++ FaceRecognitionProject/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInClicked(_ sender: UIButton) {
+        let authContext = LAContext()
+        
+        var error:NSError?
+        
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Is it you?") { success, error in
+                DispatchQueue.main.async {
+                    if success == true {
+                            self.performSegue(withIdentifier: "toSecondVC", sender: nil)
+                            
+                        } else {
+                            self.myLabel.text = "Error!"
+                        }
+                    }
+            }
+        }
+    }
+    
 }
 
