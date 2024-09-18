61a908a64996adef70ddb7c6b8a3a8a76b432324
far1h <farihmuhammad45@gmail.com>
2024-09-18 10:45:37 +0700

    Initial Commit

---
+++ FaceRecognitionProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceRecognitionProject
+//
+//  Created by Farih Muhammad on 18/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
